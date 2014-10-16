804548a7b5ab77269a4d1be94032b52d0cdc80a2
Jon Fowler <fowler.js@gmail.com>
2014-10-16 10:38:47 +0100

    basic main

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,26 @@
+
 import Reach.Parser.Parser
 import Reach.Parser.Conv
-import Control.Monad.Except
 import Reach.Eval.Basic
-import Reach.Env
+import Reach.Eval
 
+import System.Environment
+import System.Console.GetOpt
+
+main :: IO ()
 main = do
-  a <- readProg "/home/jon/Github/reach/src/test.txt"
+  (fn:_) <- getArgs
+  go fn 
+
+go :: FilePath -> IO ()
+go fn = do
+  a <- readProg fn
   case runExcept $ conv a of
     Left e -> print e
     Right fs -> do
       let (e,s) = toProg fs
-      print (fst $ normalB e s)      
+      case fst $ normalB e s of
+        Left err -> print err
+        Right e -> putStrLn . printVal $ e
 
 
