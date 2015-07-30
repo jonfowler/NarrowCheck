45ee86946305237b8b2bad61b190e69d54f49a92
Jonathan Fowler <fowler.js@gmail.com>
2015-07-30 15:16:49 +0100

    work on parser

--- src/Reach/Parser/Combinator.hs
+++ src/Reach/Parser/Combinator.hs
@@ -1,7 +1,33 @@
-module Combinator where
+module Reach.Parser.Combinator where
 
-open import Data.Maybe
+import Data.Maybe
+import Control.Arrow
+import Control.Applicative
 
-data Parse a = Parse (String -> Maybe a)
+data Parse t a = Parse {runParse :: ([t] -> Maybe (a , [t]))}
+
+instance Functor (Parse t) where
+  fmap f (Parse p) = Parse $ fmap (first f) . p
+
+instance Applicative (Parse t) where
+  pure a = Parse (\s -> Just (a , s))
+  Parse p <*> Parse p' = Parse $ \s ->
+    p s >>= \(f , s') -> fmap (first f) (p' s')
+
+instance Monad (Parse t) where
+  return = pure
+  Parse p >>= m = Parse $ \s ->
+    p s >>= \(a , s') -> runParse (m a) s'
+
+instance Alternative (Parse t) where
+  empty = Parse (\s -> Nothing)
+  Parse p <|> Parse p' = Parse $ \s ->
+    p s <|> p' s
+
+char :: Char -> Parse Char () 
+char a = Parse $ \s -> case s of
+  [] -> Nothing
+  (a' : s') -> if a == a' then Just (() , s') else Nothing
 
 
+  
