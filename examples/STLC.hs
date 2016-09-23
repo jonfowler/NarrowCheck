45cbb9ac712361510e6f6f07b6b1f6dcd511caa1
Jonathan Fowler <fowler.js@gmail.com>
2016-09-23 22:39:01 +0100

    work on STLC

--- examples/STLC.hs
+++ examples/STLC.hs
@@ -1,6 +1,6 @@
 module STLC where
 
-import Prelude ()
+import Prelude (Show)
 import OverlapPrelude
 
 data Expr = Add Expr Expr
@@ -12,10 +12,17 @@ data Expr = Add Expr Expr
             -- this can be thrown away after
           | App Type Expr Expr
           | Lam Expr
-            
-data Type = TypeN | TypeB | Fun Type Type | NoType
+          | Error deriving (Show)
 
-data Eval = Val Expr | Error
+{-# DIST If 2 #-}
+{-# DIST Add 2 #-}
+{-# DIST App 2 #-}
+{-# DIST Lam 1 #-}
+{-# DIST N 1 #-}
+{-# DIST B 1 #-}
+            
+data Type = TypeN | TypeB | Fun Type Type | NoType deriving Show
+
 
 TypeN =|= TypeN = True
 TypeB =|= TypeB = True
@@ -24,20 +31,20 @@ a =|= b = False
 
 data TList = C Type TList | E
 
-closed' :: Expr -> Nat -> Bool
-closed' (Add e e') n = closed' e n && closed' e' n
-closed' (If e e' e'') n = closed' e n && closed' e' n && closed' e'' n 
-closed' (App t e e') n = closed' e n && closed' e' n
-closed' (Lam e) n = closed' e (S n) 
-closed' (Var v) n = v <= n
-closed' m n = True
-
-closed :: Expr -> Bool
-closed e = closed' e Z 
+--closed' :: Expr -> Nat -> Bool
+--closed' (Add e e') n = closed' e n && closed' e' n
+--closed' (If e e' e'') n = closed' e n && closed' e' n && closed' e'' n 
+--closed' (App t e e') n = closed' e n && closed' e' n
+--closed' (Lam e) n = closed' e (S n) 
+--closed' (Var v) n = v <= n
+--closed' m n = True
+--
+--closed :: Expr -> Bool
+--closed e = closed' e Z 
 
 oftype :: Expr -> Type -> TList ->  Bool
 oftype (Add e e') TypeN ts = oftype e TypeN ts && oftype e' TypeN ts
-oftype (If e e' e'') t ts = oftype e t ts && oftype e' t ts && oftype e'' t ts
+oftype (If e e' e'') t ts = oftype e TypeB ts && oftype e' t ts && oftype e'' t ts
 oftype (N n) TypeN ts = True 
 oftype (B b) TypeB ts = True
 oftype (Var v) t ts = oftypeVar v t ts
@@ -55,19 +62,70 @@ depthExpr (If e e' e'') = S (max (depthExpr e)
                             (max (depthExpr e')
                                  (depthExpr e'')))
 depthExpr (App t' e e') = S (max (depthExpr e) (depthExpr e'))
-depthExpr (Lam e) = depthExpr e
+depthExpr (Lam e) = (depthExpr e)
 depthExpr x = Z
 
-subst :: Nat -> Expr -> Expr -> Expr
-subst n e (Var v) = if (v < n)
-                       (Var v)
-                       (if (v == n)
-                           (  ))
-subst n e (Lam e') = subst (S n) e e'
+rename :: (Nat -> Nat) -> Expr -> Expr
+rename f (Add e e') = Add (rename f e) (rename f e')
+rename f (If e e' e'') = If (rename f e) (rename f e') (rename f e'')
+rename f (App t' e e') = App t' (rename f e) (rename f e')
+rename f (Lam e) = Lam (rename (extendRename f) e)
+rename f (Var v) = Var (f v) 
+rename f e = e
+
+extendRename :: (Nat -> Nat) -> Nat -> Nat 
+extendRename f Z = Z
+extendRename f (S v) = S (f v)
+
+extendSubst :: (Nat -> Expr) -> Nat -> Expr
+extendSubst f Z = Var Z
+extendSubst f (S v) = rename S (f v)
+
+subst :: (Nat -> Expr) -> Expr -> Expr
+subst f (Add e e') = Add (subst f e) (subst f e')
+subst f (If e e' e'') = If (subst f e) (subst f e') (subst f e'')
+subst f (App t' e e') = App t' (subst f e) (subst f e')
+subst f (Lam e) = Lam (subst (extendSubst f) e)
+subst f (Var v) = f v
+subst f e = e
+
+notFunc :: Type -> Bool
+notFunc TypeN = True
+notFunc TypeB = True
+notFunc n = True
+
+subZ :: Expr -> Expr -> Expr
+subZ e e' = subst (repZ e) e'
+
+repZ :: Expr -> Nat -> Expr 
+repZ e Z = e
+repZ e (S v) = Var v
+
+eval :: Expr -> Expr
+eval (App t e e') = evalApp (eval e) e'
+eval (Add e e') = evalAdd (eval e) (eval e')
+eval (If e e' e'') = evalIf (eval e) e' e'' 
+eval e = e
+
+noError :: Expr -> Bool
+noError Error = False
+noError e = True
+
+evalApp (Lam e) e' = eval (subZ e' e)
+evalApp e e' = Error
+
+evalAdd :: Expr -> Expr -> Expr
+evalAdd (N n) (N n') = N (n + n')
+evalAdd e e' = Error
+
+evalIf (B True) e e' = eval e
+evalIf (B False) e e' = eval e'
+evalIf e e' e'' = Error
 
 check :: Expr -> Type -> Result
-check e t = oftype e t E && (depthExpr e <= s5)
-                 ==> True
+check e t = oftype e t E && notFunc t && (depthExpr e <= s3)
+                 ==> noError (eval e)
+
 
 {-# DIST Add 3 #-}
 {-# DIST If 3 #-}
